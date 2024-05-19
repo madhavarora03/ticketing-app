0beea6bbe29b609b2c39e881e326d81e688c4d9b
madhavarora03 <eragon.cube@outlook.com>
2024-05-19 15:24:43 +0530

    Adding password hashing and completing signup route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
@@ -21,6 +23,17 @@ app.all("*", () => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on 3000!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on 3000!!!");
+  });
+};
+
+start();
