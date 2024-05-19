9d4641ff1947014c8b7600aec26b636a4b1a5ad8
madhavarora03 <eragon.cube@outlook.com>
2024-05-19 07:59:00 +0530

    Adding custom error implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import "express-async-errors";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -14,6 +16,9 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
+app.all("*", () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {
