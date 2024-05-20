6d7a8f80086a8d254a5342a43431b01cab21b800
madhavarora03 <eragon.cube@outlook.com>
2024-05-20 15:36:30 +0530

    Complete signup

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,53 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../middleware/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid!"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password must be provided!"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials!");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password,
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    req.session = { jwt: userJwt };
+
+    return res.status(200).send(existingUser);
+  },
+);
 
 export { router as signinRouter };
