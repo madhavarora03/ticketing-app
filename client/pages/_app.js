2fa947ddba876ec4ca2dbe68cf4a29b84d3b2ced
madhavarora03 <eragon.cube@outlook.com>
2024-06-04 20:54:04 +0530

    Handling getinitial props for different pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,22 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default function Layout({ Component, pageProps }) {
+function AppComponent({ Component, pageProps, currentUser }) {
   return <Component {...pageProps} />;
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
