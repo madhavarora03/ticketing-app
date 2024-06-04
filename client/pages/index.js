3c5be66e6ea6105d88184943238b1fcb5c33d837
madhavarora03 <eragon.cube@outlook.com>
2024-06-04 16:23:43 +0530

    Getting current user from getInitialProps in nextjs

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,30 @@
-export default function Page() {
+import axios from "axios";
+
+function LandingPage({ currentUser }) {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 }
+
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        },
+      )
+      .catch((err) => console.log(err.message));
+
+    return data;
+  } else {
+    const { data } = await axios
+      .get("/api/users/currentuser")
+      .catch((err) => console.log(err.message));
+
+    return data;
+  }
+};
+
+export default LandingPage;
