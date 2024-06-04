4f39535fcacded77a62450faefd98c554e47f294
madhavarora03 <eragon.cube@outlook.com>
2024-06-04 16:46:47 +0530

    Adding buildClient for handling server and client api-calls

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,30 +1,16 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 function LandingPage({ currentUser }) {
   console.log(currentUser);
+
   return <h1>Landing page</h1>;
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === "undefined") {
-    const { data } = await axios
-      .get(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: req.headers,
-        },
-      )
-      .catch((err) => console.log(err.message));
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
 
-    return data;
-  } else {
-    const { data } = await axios
-      .get("/api/users/currentuser")
-      .catch((err) => console.log(err.message));
-
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
