f80252fa582fc99df082e8c9f4fa12538e751385
madhavarora03 <eragon.cube@outlook.com>
2024-06-19 06:27:29 +0530

    Adding validation logic in new ticket route

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
-import { requireAuth } from "@my-ticketing-app/common";
+import { requireAuth, validateRequest } from "@my-ticketing-app/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price Must be greater than zero"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  },
+);
 
 export { router as createTicketRouter };
